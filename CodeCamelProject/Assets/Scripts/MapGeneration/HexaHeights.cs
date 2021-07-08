77d0cdb12832a2c771b88c2ed079814676bd9573
Philémon MOREL <pro.pmorel@gmail.com>
2021-07-08 13:29:26 +0100

    Creation of the Mouvement Hexa Grid

---
+++ CodeCamelProject/Assets/Scripts/MapGeneration/HexaHeights.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HexaHeights : MonoBehaviour
+{
+    [SerializeField] private float gameHeight;
+    [SerializeField] private float speed;
+    [SerializeField] private List<GameObject> listHexa;
+    [SerializeField] private List<float> startHexa;
+
+    private bool allow; 
+
+    private void Awake()
+    {
+        allow = false;
+
+        for (int i = 0; i < listHexa.Count; i++)
+        {
+            startHexa.Add(listHexa[i].transform.position.y);
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            allow = true;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Backspace))
+        {
+            allow = false;
+        }
+
+
+        if (allow && listHexa[0].transform.position.y != gameHeight)
+        {
+            for (int i = 0; i < listHexa.Count; i++)
+            {
+                Vector3 vector3 = new Vector3(listHexa[i].transform.position.x, gameHeight, listHexa[i].transform.position.z);
+                listHexa[i].transform.position = Vector3.Lerp(listHexa[i].transform.position, vector3, speed);
+            }
+        }
+        else if (!allow)
+        {
+            for (int i = 0; i < listHexa.Count; i++)
+            {
+                Vector3 vector3 = new Vector3(listHexa[i].transform.position.x, startHexa[i], listHexa[i].transform.position.z);
+                listHexa[i].transform.position = Vector3.Lerp(listHexa[i].transform.position, vector3, speed);
+            }
+        }
+    }
+}
